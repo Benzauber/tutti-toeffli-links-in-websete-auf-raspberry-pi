f591e5598eabfb1245c4972a5cb76ffb9a4e45ef
Benzauber <88102108+Benzauber@users.noreply.github.com>
2024-01-13 14:23:13 +0100

    Create delete.py

---
+++ delete.py
@@ -0,0 +1,14 @@
+# Datei einlesen
+with open('/var/www/html/links.txt', 'r') as file:
+    lines = file.readlines()
+
+# Erste 5 und letzten 30 löschen
+cleaned_lines = lines[5:-41]
+
+# Zweite Links löschen
+cleaned_lines = [line for i, line in enumerate(cleaned_lines) if i % 2 != 0]
+
+# Inhalt in die gleiche Datei schreiben
+with open('/var/www/html/links.txt', 'w') as file:
+    file.writelines(cleaned_lines)
+
