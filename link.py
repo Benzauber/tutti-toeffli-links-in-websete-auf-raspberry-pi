46ec3810737a868f74471f8cf6d897c644a96db3
Benzauber <88102108+Benzauber@users.noreply.github.com>
2024-01-13 17:51:24 +0100

    Update link.py

--- link.py
+++ link.py
@@ -1,35 +1,32 @@
-import requests
-from bs4 import BeautifulSoup
-from urllib.parse import urljoin
-import time
-
-while True:
-    # URL der Webseite, die du durchsuchen möchtest
-    url = 'https://www.tutti.ch/de/q/suche/Ak6d0w7ZmZmxpwJTAwJGUpXByaWNlws0CvMDA?sorting=newest&page=1&query=t%C3%B6ffli'
-
-    # Anforderung an die Webseite senden
-    response = requests.get(url)
-
-    if response.status_code == 200:
-        soup = BeautifulSoup(response.text, 'html.parser')
-
-        links = soup.find_all('a')
-
-        with open('/var/www/html/links.txt', 'w') as file:
-            for link in links:
-                href = link.get('href')
-                if href is not None:
-                    full_url = urljoin(url, href)
-                    file.write(full_url + '\n')
-
+import requests  
+from bs4 import BeautifulSoup  
+from urllib.parse import urljoin  
+  
+# URL der Webseite, die du durchsuchen möchtest  
+url = 'https://www.tutti.ch/de/q/suche/Ak6d0w7ZmZmxpwJTAwJGUpXByaWNlws0CvMDA?sorting=newest&page=1&query=t%C3%B6ffli'  
+  
+# Anforderung an die Webseite senden  
+response = requests.get(url)  
+  
+if response.status_code == 200:  
+   soup = BeautifulSoup(response.text, 'html.parser')  
+      
+   links = soup.find_all('a')  
+  
+   with open('/var/www/html/links.txt', 'w') as file:  
+       for link in links:  
+           href = link.get('href')  
+           if href is not None:  
+               full_url = urljoin(url, href)  
+               file.write(full_url + '\n')  
         # Hauptdatei (zum Beispiel hauptdatei.py)
         with open("delete.py", "r") as f:
             code = f.read()
             exec(code)
 
-        print('Links wurden erfolgreich in links.txt gespeichert.')
-    else:
-        print('Fehler beim Abrufen der Webseite. Statuscode:', response.status_code)
+  
+  
+   print('Links wurden erfolgreich in links.txt gespeichert.')  
+else:  
+   print('Fehler beim Abrufen der Webseite. Statuscode:', response.status_code)
 
-    # Warte 10 Minuten, bevor das Skript erneut ausgeführt wird
-    time.sleep(600)  # 600 Sekunden entsprechen 10 Minuten
