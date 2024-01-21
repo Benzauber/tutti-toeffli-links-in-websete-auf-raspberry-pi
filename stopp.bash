95188f952edd4a35912cbcfe702105b7bed5a2bd
Benzauber <88102108+Benzauber@users.noreply.github.com>
2024-01-21 11:19:26 +0100

    Update stopp.bash

--- stopp.bash
+++ stopp.bash
@@ -1,1 +1,14 @@
+#!/bin/bash
 
+# stopp.bash
+
+if [ -f "process.pid" ]; then
+    pid=$(cat process.pid)
+    # Beenden des Prozesses mit der gespeicherten PID
+    kill -9 "$pid"
+    echo "Prozess mit der PID $pid wurde beendet."
+    # Löschen der PID-Datei
+    rm process.pid
+else
+    echo "Die PID-Datei existiert nicht. Der Prozess wurde möglicherweise bereits beendet."
+fi
