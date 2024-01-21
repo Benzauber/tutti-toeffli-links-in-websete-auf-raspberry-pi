5158a7e9ac94160e8edf2acf95968dde5638b0b6
Benzauber <88102108+Benzauber@users.noreply.github.com>
2024-01-21 19:31:27 +0100

    Update install.bash

--- install.bash
+++ install.bash
@@ -1,20 +1,20 @@
 #!/bin/bash
 
 echo "Wird installiert"
-sudo apt-get update
-sudo apt-get install python3-pip
+apt-get update
+apt-get install python3-pip
 python3 -m venv myenv
 source myenv/bin/activate
 pip install beautifulsoup4
 pip install requests
 
 echo "Webseite wird eingerichtet"
-sudo apt-get install apache2
-sudo chmod a+w /var/www/html/
+apt-get install apache2
+chmod a+w /var/www/html/
 
-sudo rm /var/www/html/index.html
+rm /var/www/html/index.html
 echo "/var/www/html/index.html wurde gelöscht"
-sudo cp index.html /var/www/html/
+cp index.html /var/www/html/
 echo "index.html wurde nach /var/www/html/ kopiert"
 
 bash auto.bash
