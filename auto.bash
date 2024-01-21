d2e18c2d9db651afce379bc3fe3b5250575434a9
Benzauber <88102108+Benzauber@users.noreply.github.com>
2024-01-21 11:16:34 +0100

    Update auto.bash

--- auto.bash
+++ auto.bash
@@ -1,13 +1,23 @@
-#!/bin/bash  
-  
-# Benutzername ermitteln  
-USERNAME=$(whoami)  
-  
-# Inhalt des Crontab-Eintrags für 14:20 Uhr  
-CRON_ENTRY="0 0 * * * /usr/bin/python3 $HOME/tutti-toeffli-links-in-websete-auf-paspberry-pi/link.py"  
-  
-# (Optional) Crontab-Befehl sichern  
-(crontab -l ; echo "$CRON_ENTRY") | crontab -  
-  
-# Optional: Crontab-Dienst neu starten  
-sudo service cron restart
+#!/bin/bash
+
+while true; do
+  echo "Möchten Sie die Automation starten? (Y/n)"
+  read -r input
+
+  if [ -z "$input" ] || [ "$input" == "Y" ] || [ "$input" == "y" ]; then
+    echo "start.bash wird ausgefürt"
+    echo "Um es zu stoppen 'bash stop.bash'"
+    bash start.bash
+    # Hier können Sie den Code für den Fall einfügen, dass die Antwort Ja oder Enter ist.
+    break  # Beende die Schleife, da eine gültige Eingabe erfolgt ist.
+  elif [ "$input" == "N" ] || [ "$input" == "n" ]; then
+    echo "Nicht ausgefürt!"
+    echo "Um es auszuführen 'bash start.bash'"
+    # Hier können Sie den Code für den Fall einfügen, dass die Antwort Nein ist.
+    break  # Beende die Schleife, da eine gültige Eingabe erfolgt ist.
+  else
+    echo "Ungültige Eingabe. Bitte verwenden Sie Y oder N."
+    # Hier können Sie den Code für den Fall einfügen, dass die Eingabe ungültig ist.
+  fi
+done
+
