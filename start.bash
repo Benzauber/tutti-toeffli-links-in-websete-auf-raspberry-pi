5018e8b01426295e6d4d75295b66834c50a7a949
Benzauber <88102108+Benzauber@users.noreply.github.com>
2024-01-21 12:01:03 +0100

    Update start.bash

--- start.bash
+++ start.bash
@@ -3,5 +3,5 @@
 # start.bash
 
 # Speichern der Prozess-ID in einer Datei
-nohup bash -c 'while true; do python link.py; sleep 60; done' > output.log 2>&1 &
+nohup bash -c 'while true; do python link.py; sleep 600; done' > output.log 2>&1 &
 echo $! > process.pid
